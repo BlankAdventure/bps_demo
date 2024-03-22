f81c23f7c808ef982f7fbe3ef02e38aad573f6c1
BlankAdventure <>
2024-03-22 17:55:28 -0400

    try with queue

--- bps_demo.py
+++ bps_demo.py
@@ -19,7 +19,12 @@ from nicegui import ui, run
 import asyncio
 
 
-
+async def worker(name, queue):
+    while True:
+        res = await queue.get()
+        res()
+        #await make_async(res)()
+        queue.task_done()
 
 def make_async(function_to_decorate):
     async def async_wrap(*args,**kwargs):
@@ -97,127 +102,30 @@ class psdClass():
 check_in_range = lambda x,r: any(lower <= x <= upper for (lower, upper) in r)
 clr_dict = {True: 'limegreen', False: 'r'}
 
-# class BandpassApp():
-#     def __init__(self, fc=3500, bw=1000, dur=5, npsd=-60):
-#         self.dur = dur 
-#         self.npsd = npsd 
-        
-#         # Dynamic plot elements
-#         self.line1 = None
-#         self.line2 = None
-#         self.axvline1 = None
-#         self.axvline2 = None
-#         self.calc_base_vals(fc,bw)
-#         self.setup_ui()
-    
-#     # Calculates basic simulation parameters
-#     def calc_base_vals(self, fc, bw):
-#         self.fc = fc # RF carrier freq, Hz
-#         self.bw = bw # Signal bandwidth, Hz
-#         self.fu = fc + bw/2
-#         self.fl = fc - bw/2
-#         self.min_fs = bw*2
-#         self.base_fs = self.fu*3
-#         self.ranges = calc_valid_ranges(self.base_fs, self.fc, self.bw)        
-    
-#     # Setup the plot and UI elements. This function only called once. Afterwards
-#     # we only update the plot (update_plot) or the static elements (update_static)
-#     def setup_ui(self):
-#         self.base_ff, self.base_psd  = get_psd( self.base_fs, self.fc, self.fl, self.fu, self.dur, self.npsd)       
-#         with ui.card().classes('bg-yellow-50'):
-#             with ui.column().classes('gap-0'): 
-#                 ui.label('Bandpass Sampling Demo').style('font-size: 120%; font-weight: bold;').classes('w-full text-center')
-#                 self.title = ui.label(f'(fc={self.fc:.0f} Hz | BW={self.bw:.0f} Hz | fmax={self.fu:.0f} Hz)').style('font-size: 90%; font-weight: normal;').classes('w-full text-center')
-#                 self.main_plot = ui.pyplot(figsize=(9, 5))
-#                 with self.main_plot:
-#                     self.main_plot.fig.patch.set_alpha(0)                    
-#                     self.line1, = plt.plot(self.base_ff,self.base_psd, color='k')
-#                     self.line2, = plt.plot(self.base_ff,self.base_psd, color=clr_dict[check_in_range(self.fu*2,self.ranges)])          
-#                     self.axvline1 = plt.axvline( self.base_fs/2, linestyle='--', color='grey', alpha=0.60)
-#                     self.axvline2 = plt.axvline(-self.base_fs/2, linestyle='--', color='grey', alpha=0.60)
-#                     ax = plt.gca()
-#                     ax.patch.set_alpha(0)
-#                     ax.spines['right'].set_visible(False)
-#                     ax.spines['top'].set_visible(False)
-#                     ax.spines['left'].set_visible(True)
-#                     ax.spines['bottom'].set_visible(True)
-#                     plt.xlim(-self.base_fs/2,self.base_fs/2)
-#                     plt.ylim(-65, -25)                    
-#                     plt.xlabel('Freq [Hz]')
-#                     plt.ylabel('PSD [dBW/Hz]')
-#                     plt.margins(x=0,y=0,tight=True)                    
-#                     self.main_plot.fig.tight_layout()
-                
-#                 # Plot annotation of aliasinsg zones
-#                 self.regions = ui.label(build_str((self.ranges))).classes().style("position: absolute; top: 13%; left: 12%; white-space: pre; font-size: 85%; font-weight: 500; border: 1px solid; padding: 3px; background-color: rgba(249, 250, 251, 0.88);")
-                
-#                 # Setup the slider
-#                 ui.label('Sampling Rate [Hz]:').classes('text-left italic')
-#                 self.samp_slider = ui.slider(min=self.min_fs, max=self.base_fs, step=5, value=self.base_fs).props('label-always') \
-#                     .on('update:model-value', lambda e: self.update_test(e.args),throttle=1,leading_events=False).classes('w-full').props()
-                
-#                 # Setup the indicator bar
-#                 self.zonebar =  ui.row().classes('w-full gap-0 bg-red-300').style('position: relative; top: -10px;') 
-#                 self.build_zonebar()
-            
-#                 with ui.row().classes('w-full items-center justify-left'):
-#                     ui.label('Carrier Freq [Hz]:').classes('italic')
-#                     ui.slider(min=2500,max=4500,step=50,value=self.fc).style('width: 35%;').props('label-always switch-label-side').on('update:model-value', lambda e: self.update_ref(e.args,self.bw),throttle=1,leading_events=False)
-#                     ui.label('Bandwidth [Hz]:').classes('italic')
-#                     ui.slider(min=500,max=1500,step=50,value=self.bw).style('width: 35%;').props('label-always switch-label-side').on('update:model-value', lambda e: self.update_ref(self.fc,e.args),throttle=1,leading_events=False)
-        
-#     # Helper function to draw alias region color bar
-#     def build_zonebar(self):
-#         with self.zonebar:
-#             ofs = 0
-#             w = 0
-#             for L,U in reversed(self.ranges):
-#                 ofs = ofs + w
-#                 w = 100*(U-L)/(self.base_fs - self.min_fs)
-#                 p = 100*(L-self.min_fs)/(self.base_fs - self.min_fs)
-#                 if w == 0:
-#                     w = 0.1
-#                 ui.element('div').classes('bg-green-400').style(f'position: relative; left: {p-ofs}%; height: 15px; width: {w}%;')
-    
-    
-#     # Update static plot elements for new fc or bw values
-#     async def update_ref(self, fc, bw):
-#         self.calc_base_vals(fc, bw)
-#         self.title.set_text(f'(fc={self.fc:.0f} Hz | BW={self.bw:.0f} Hz | fmax={self.fu:.0f} Hz)')
-#         self.regions.set_text(build_str(self.ranges))
-#         self.samp_slider.props(f'markers :min={self.min_fs}')
-#         self.samp_slider.props(f'markers :max={self.base_fs}')        
-#         if self.samp_slider.value > self.base_fs:
-#             self.samp_slider.value = self.base_fs        
-#         self.zonebar.clear()
-#         self.build_zonebar()        
 
-#         ff, Pdb  = await make_async( lambda: get_psd( self.base_fs, self.fc, self.fl, self.fu, self.dur, self.npsd)  )()
-
-#         with self.main_plot:
-#             self.line1.set_xdata(self.base_ff)
-#             self.line1.set_ydata(self.base_psd)
-#             plt.xlim(-self.base_fs/2,self.base_fs/2)        
-#         await self.update_test(self.samp_slider.value)     
+def test_func(x):
+    print(f'this was called with {x}')
     
-    
-#     # Redraw the plot when provided a new fs    
-#     async def update_test(self, fs):
-#         ff, Pdb  = await make_async( lambda: get_psd( fs, self.fc, self.fl, self.fu, self.dur, self.npsd)  )()
-#         with self.main_plot:
-#             self.line2.set_xdata(ff)
-#             self.line2.set_ydata(Pdb)
-#             self.line2.set( color=clr_dict[check_in_range(fs,self.ranges)] )
-#             self.axvline1.set_data([fs/2, fs/2], [0, 1])
-#             self.axvline2.set_data([-fs/2, -fs/2], [0, 1])
-
-
 
 
 @ui.page('/main')
 def main():
     
     psd = psdClass()
+    queue = asyncio.LifoQueue(maxsize=1)
+    
+    async def ur(fc, bw):
+        try: 
+            queue.put_nowait( lambda: update_ref_psd(fc, bw) )
+        except:
+            print('q full')
+
+    async def ut(fs):
+        try: 
+            queue.put_nowait( lambda: update_test_psd(fs) )
+        except:
+            print('q full')
+
 
     def draw_zonebar():
         with zone_bar:
@@ -289,7 +197,7 @@ def main():
             # Setup the slider
             ui.label('Sampling Rate [Hz]:').classes('text-left italic')
             samp_slider = ui.slider(min=psd.min_fs, max=psd.base_fs, step=5, value=psd.base_fs).props('label-always') \
-                .on('update:model-value', lambda e: update_test_psd(e.args),throttle=1,leading_events=False).classes('w-full').props()
+                .on('update:model-value', lambda e: ut(e.args),throttle=1,leading_events=False).classes('w-full').props()
             
             # Setup the indicator bar
             with ui.row().classes('w-full gap-0 bg-red-300').style('position: relative; top: -10px;') as zone_bar:
@@ -297,12 +205,12 @@ def main():
         
             with ui.row().classes('w-full items-center justify-left'):
                 ui.label('Carrier Freq [Hz]:').classes('italic')
-                ui.slider(min=2500,max=4500,step=50,value=psd.fc).style('width: 35%;').props('label-always switch-label-side').on('update:model-value', lambda e: update_ref_psd(e.args,psd.bw),throttle=1,leading_events=False)
+                ui.slider(min=2500,max=4500,step=50,value=psd.fc).style('width: 35%;').props('label-always switch-label-side').on('update:model-value', lambda e: ur(e.args,psd.bw),throttle=1,leading_events=False)
                 ui.label('Bandwidth [Hz]:').classes('italic')
-                ui.slider(min=500,max=1500,step=50,value=psd.bw).style('width: 35%;').props('label-always switch-label-side').on('update:model-value', lambda e: update_ref_psd(psd.fc,e.args),throttle=1,leading_events=False)
+                ui.slider(min=500,max=1500,step=50,value=psd.bw).style('width: 35%;').props('label-always switch-label-side').on('update:model-value', lambda e: ur(psd.fc,e.args),throttle=1,leading_events=False)
 
 
-        
+    task = asyncio.create_task(worker('name', queue))        
     
     
 
